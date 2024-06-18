fd4b3e79da7e5ea597816414d2042d464873866d
HieuTBT <trunghieu17082001@gmail.com>
2024-06-18 08:55:48 +0700

    nop bai tap

---
+++ management_student.sql
@@ -0,0 +1,19 @@
+create database student_management;
+
+use student_management;
+
+create table Class(
+	class_id int,
+    class_name varchar(200)
+);
+
+create table Teacher(
+	teacher_id int,
+    teacher_name varchar(200),
+    age int,
+    country varchar(200)
+);
+
+
+
+
